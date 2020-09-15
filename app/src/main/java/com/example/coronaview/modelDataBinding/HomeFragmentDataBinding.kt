213bcc26f574210b54c74e3b0a75d53ef08f79f4
Mario <heberfrd1@gmail.com>
2020-09-15 19:44:13 -0300

    Adicionando DataBinding ao projeto. Implementando na tela de Home

---
+++ app/src/main/java/com/example/coronaview/modelDataBinding/HomeFragmentDataBinding.kt
@@ -0,0 +1,11 @@
+package com.example.coronaview.modelDataBinding
+
+data class HomeFragmentDataBinding(
+    var status : Boolean,
+    var casosNovos : String,
+    var casosTotais : String,
+    var obitosNovos : String,
+    var obitosTotais : String,
+    var recuperadosNovos : String,
+    var recuperadosTotais : String,
+    var ultimaAtt : String)
--- app/src/main/java/com/example/coronaview/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/coronaview/ui/fragments/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.example.coronaview.ui.fragments
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -17,12 +18,15 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.databinding.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.example.coronaview.BR
 import com.example.coronaview.R
 import com.example.coronaview.common.Response
 import com.example.coronaview.common.Status
 import com.example.coronaview.data.api.model.CoronaEstatisticas
+import com.example.coronaview.modelDataBinding.HomeFragmentDataBinding
 import com.example.coronaview.ui.viewModel.EstatisticasViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.fragment_principal.*
@@ -30,12 +34,22 @@ import java.io.File
 import java.io.FileOutputStream
 import java.util.*
 
-
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(){
 
     companion object {
         fun newInstance() = HomeFragment()
+        @set:BindingAdapter("bind:isVisible")
+        @JvmStatic
+        var View.isVisible:Boolean
+            get() = visibility == View.VISIBLE
+            set(value){
+                visibility = if (value) View.VISIBLE else View.INVISIBLE
+            }
     }
+
+    private lateinit var binding: ViewDataBinding
+    private var homeBinding = getEmptyHomeFragmentDataBinding()
+    //private var textViewUltimaAtt: String = ""
 
     val viewModel = EstatisticasViewModel()
 
@@ -48,13 +62,17 @@ class HomeFragment : Fragment() {
             })
         }
         viewModel.getCoronaEstatisticas()
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_principal, container, false)
+        var v = inflater.inflate(R.layout.fragment_principal, container, false)
+        binding = DataBindingUtil.bind(v)!!
+        binding.setVariable(BR.data,homeBinding)
+        return binding.getRoot()
     }
 
     private fun processResponse(response: Response, contexto : Context) {
@@ -67,94 +85,24 @@ class HomeFragment : Fragment() {
 
     @SuppressLint("SetTextI18n")
     private fun responseSuccess(result : Any?) {
-
+        
         var coronaEstatisticas = result as List<CoronaEstatisticas>
 
-        var textViewUltimaAtt =
-            requireActivity().findViewById<TextView>(R.id.textViewUltimaAtualizacao)
-
-        var textViewCasosNovos =
-            requireActivity().findViewById<TextView>(R.id.textViewNumeroNovosCasos)
-        var textViewCasosTotais =
-            requireActivity().findViewById<TextView>(R.id.textViewNumeroTotalCasos)
-
-        var textViewObitosNovos =
-            requireActivity().findViewById<TextView>(R.id.textViewNumeroNovosObitos)
-        var textViewObitosTotais =
-            requireActivity().findViewById<TextView>(R.id.textViewNumeroTotalObitos)
-
-        var textViewRecuperadosNovos =
-            requireActivity().findViewById<TextView>(R.id.textViewNumeroNovosRecuperados)
-        var textViewRecuperadosTotais =
-            requireActivity().findViewById<TextView>(R.id.textViewNumeroTotalRecuperados)
-
-        textViewCasosNovos.text = "\n" + viewModel.coronaEstatisticasRepository.getNovosCasos(
-            coronaEstatisticas[0],
-            coronaEstatisticas[1]
-        ).toString()
-        textViewObitosNovos.text = "\n" + viewModel.coronaEstatisticasRepository.getNovosObitos(
-            coronaEstatisticas[0],
-            coronaEstatisticas[1]
-        ).toString()
-        textViewRecuperadosNovos.text =
-            "\n" + viewModel.coronaEstatisticasRepository.getNovosRecuperados(
-                coronaEstatisticas[0],
-                coronaEstatisticas[1]
-            ).toString()
-
-        textViewCasosTotais.text = "\n" + coronaEstatisticas[0].infected.toString()
-        textViewObitosTotais.text = "\n" + coronaEstatisticas[0].deceased.toString()
-        textViewRecuperadosTotais.text = "\n" + coronaEstatisticas[0].recovered.toString()
-
-        textViewUltimaAtt.text = "Atualizado em " +
-                viewModel.coronaEstatisticasRepository.getUltimoUpDateFromlastUpdatedAtSource(
-                    coronaEstatisticas[0].lastUpdatedAtSource!!
-                ) +
-                "\n- Última atualização disponível -"
-
-        cardViewCasos.visibility = View.VISIBLE
-        cardViewObitos.visibility = View.VISIBLE
-        cardViewRecuperados.visibility = View.VISIBLE
-        textViewUltimaAtt.visibility = View.VISIBLE
-
-        progressBarRequisicao.visibility = View.GONE
-
-        /*var fab = requireActivity().findViewById<FloatingActionButton>(R.id.fab)
-        fab.setOnClickListener {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf<String>(WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE),
-                PackageManager.PERMISSION_GRANTED
-            )
-            val builder: StrictMode.VmPolicy.Builder = StrictMode.VmPolicy.Builder()
-            StrictMode.setVmPolicy(builder.build())
-
-            val view1: View = requireActivity().window.decorView.rootView
-            view1.isDrawingCacheEnabled = true
-            val bitmap = Bitmap.createBitmap(view1.drawingCache)
-            view1.isDrawingCacheEnabled = false
-
-            val filePath: String = Environment.getExternalStorageDirectory()
-                .toString() + "/Download/" + Calendar.getInstance().getTime().toString() + ".jpg"
-            val fileScreenshot = File(filePath)
-            var fileOutputStream: FileOutputStream? = null
-            try {
-                fileOutputStream = FileOutputStream(fileScreenshot)
-                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
-                fileOutputStream.flush()
-                fileOutputStream.close()
-            } catch (e: java.lang.Exception) {
-                e.printStackTrace()
-            }
-
-            val intent = Intent(Intent.ACTION_SEND)
-            val uri: Uri = Uri.fromFile(fileScreenshot)
-            intent.setDataAndType(uri, "image/jpeg")
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            intent.putExtra(Intent.EXTRA_STREAM, uri);
-            intent.putExtra(Intent.EXTRA_TEXT,"Baixe o app em : LINK")
-            this.startActivity(Intent.createChooser(intent,"Compartilhe"))
-        }*/
+        homeBinding.apply {
+                status = true
+                casosNovos        = "\n" + viewModel.coronaEstatisticasRepository.getNovosCasos(coronaEstatisticas[0],coronaEstatisticas[1]).toString()
+                obitosNovos       = "\n" + viewModel.coronaEstatisticasRepository.getNovosObitos(coronaEstatisticas[0],coronaEstatisticas[1]).toString()
+                recuperadosNovos  = "\n" + viewModel.coronaEstatisticasRepository.getNovosRecuperados(coronaEstatisticas[0],coronaEstatisticas[1]).toString()
+                casosTotais       = "\n" + coronaEstatisticas[0].infected.toString()
+                obitosTotais      = "\n" + coronaEstatisticas[0].deceased.toString()
+                recuperadosTotais = "\n" + coronaEstatisticas[0].recovered.toString()
+                ultimaAtt         = "Atualizado em " +
+                                    viewModel.coronaEstatisticasRepository.getUltimoUpDateFromlastUpdatedAtSource(
+                                    coronaEstatisticas[0].lastUpdatedAtSource!!) +
+                                    "\n- Última atualização disponível -"
+        }
+        binding.setVariable(BR.data,homeBinding)
+        binding.executePendingBindings()
     }
 
     private fun responseFailure(error : Throwable?) = Toast.makeText(activity,error?.message?:"Falha",Toast.LENGTH_SHORT).show()
@@ -162,6 +110,9 @@ class HomeFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
     }
+
+    fun getEmptyHomeFragmentDataBinding() : HomeFragmentDataBinding = HomeFragmentDataBinding(false,"","","","","","","")
+
 
     fun onClickFAB(){
         ActivityCompat.requestPermissions(
